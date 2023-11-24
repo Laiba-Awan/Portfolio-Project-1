fc84a0ac0fb387dd90a32d917c91553cea35890b
Laiba Jamil <laibajamil404@gmail.com>
2023-11-25 00:35:10 +0500

    Not completed yet

---
+++ script.js
@@ -0,0 +1,6 @@
+// const hamburger = document.querySelector(".hamburger");
+// const nav = document.querySelector(".nav");
+
+// function hamburgerOpen(){
+//     nav.classList.toggle(".nav-open");
+// }
